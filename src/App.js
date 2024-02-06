dded6d0677a9359b70d496574480885a4ed9dc7c
sandeeep-parajapati <6392424180sandeep@gmail.com>
2024-02-06 13:30:02 +0530

    formated

--- src/App.js
+++ src/App.js
@@ -42,37 +42,37 @@ function App() {
       <Table variant="dark"  >
         <tbody>
           <tr>
-          <td>S.N</td>
+            <td>S.N</td>
             <td>Name</td>
             <td>Email</td>
             <td>Address</td>
           </tr>
           {
-            users.map((item,i)=>
-            <tr key={i}>
-               <td>{i+1}</td>
-            <td>{item.name}</td>
-          <td>{item.email}</td>
-            <td>
-              {/*  */}
-              <Table variant="dark"  >
-        <tbody>
-              {
-                item.address.map((data)=>
+            users.map((item, i) =>
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>
+                  {/*  */}
+                  <Table variant="dark"  >
+                    <tbody>
+                      {
+                        item.address.map((data) =>
 
-                <tr>
-                  <td>{data.hm}</td>
-                  <td>{data.city}</td>
-                  <td>{data.country}</td>
-                </tr>
-                )
-              }
-              </tbody>
-              </Table>
-              {/*  */}
-            </td>
-          </tr>
-          )
+                          <tr>
+                            <td>{data.hm}</td>
+                            <td>{data.city}</td>
+                            <td>{data.country}</td>
+                          </tr>
+                        )
+                      }
+                    </tbody>
+                  </Table>
+                  {/*  */}
+                </td>
+              </tr>
+            )
           }
         </tbody>
       </Table>
